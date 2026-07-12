aa302dc72c596b044b715c8a89e6db055c9a8a35
Anmol Yadav <aanmolyadvv@gmail.com>
2026-07-13 00:45:11 +0530

    changes

---
+++ DOM/timers.js
@@ -0,0 +1,38 @@
+// setTimeout() => It runs after a specified time, it adds asynchronous behaviour to the code
+
+// const id = setTimeout(() => {
+
+//     console.log("Done");
+
+// }, 3000);
+
+// console.log(id);\
+
+
+// const timer = setTimeout(() => {
+
+//     console.log("Hello");
+
+// }, 2000);
+
+
+
+
+
+
+// setInterval() => It runs after a specified time over and over again. it also adds the asynchronous behavouir to the code.
+
+// let count = 1;
+
+// const id = setInterval(() => {
+
+//     console.log(+count);
+
+//     count++;
+
+// }, 1000);
+
+
+// setTimeout(() => {
+//     clearInterval(id);
+// }, 11000);
